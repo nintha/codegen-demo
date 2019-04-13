2d4ba9c852128549ad2b0a9ba69f5452bde63882
ninthakeey <ninthakeey@hotmail.com>
2019-04-13 14:49:35 +0800

    init

---
+++ mirror/src/main/java/top/nintha/mirror/Main.java
@@ -0,0 +1,10 @@
+package top.nintha.mirror;
+
+public class Main {
+    public static void main(String[] args) {
+//        User user = new User();
+//        System.out.println(user.getUsername());
+//        user.setUsername("Alice");
+//        System.out.println(user.getUsername());
+    }
+}
---
+++ mirror/src/main/java/top/nintha/mirror/User.java
@@ -0,0 +1,14 @@
+package top.nintha.mirror;
+
+
+import top.nintha.anno.AllArgsConstructor;
+import top.nintha.anno.Data;
+import top.nintha.anno.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String username;
+    private Integer age;
+}
